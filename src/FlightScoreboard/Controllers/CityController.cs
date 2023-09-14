72f5091aed54877d30c03342ad7361a9bbdf76d3
Marina Kholodova <marinaholodova@gmail.com>
2023-09-14 17:34:19 +0300

    Add CityController+View

---
+++ src/FlightScoreboard/Controllers/CityController.cs
@@ -0,0 +1,40 @@
+﻿using FlightScoreboard.Services.Interfaces;
+using FlightScoreboard.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightScoreboard.Controllers;
+
+public class CityController : Controller
+{
+	private readonly ICityService _cityService;
+
+	public CityController(ICityService cityService)
+	{
+		this._cityService = cityService;
+	}
+
+	public IActionResult Index()
+	{
+		var cities = this._cityService.GetAllCities();
+		return this.View(cities);
+	}
+
+	[HttpGet]
+	public IActionResult Create()
+	{
+		return this.View();
+	}
+
+	[HttpPost]
+	public IActionResult Create(CityCreateModel city)
+	{
+		this._cityService.CreateCity(city);
+		return this.RedirectToAction("Index");
+	}
+
+	public IActionResult Delete(int id)
+	{
+		this._cityService.DeleteCity(id);
+		return this.RedirectToAction("Index");
+	}
+}
