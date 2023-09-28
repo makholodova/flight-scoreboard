bac978c989140eb84d383bf593274620b4069dd4
Marina Kholodova <marinaholodova@gmail.com>
2023-09-28 16:15:13 +0300

    Add Error MVC

---
+++ src/FlightScoreboard/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using FlightScoreboard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightScoreboard.Controllers;
+
+public class ErrorController : Controller
+{
+    public IActionResult Index(ErrorModel error)
+    {
+        
+        return View(error);
+    }
+}
---
+++ src/FlightScoreboard/Models/ErrorModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FlightScoreboard.Models;
+
+public class ErrorModel
+{
+    public string ErrorMessage { get; set; }
+    public string ActionName { get; set; }
+    public string ControllerName { get; set; }
+}
