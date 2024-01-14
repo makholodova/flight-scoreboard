49a48ad42d900825ad8147b5b1a495cd8f563e80
Marina Kholodova <marinaholodova@gmail.com>
2024-01-14 15:24:52 +0300

    vue

---
+++ src/FlightScoreboard/Controllers/CityNew_2Controller.cs
@@ -0,0 +1,51 @@
+﻿using FlightScoreboard.Models;
+using FlightScoreboard.Services;
+using FlightScoreboard.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightScoreboard.Controllers;
+
+[ApiController]
+public class CityNew_2Controller : Controller
+{
+    private readonly ICityService _cityService;
+
+    public CityNew_2Controller(ICityService cityService)
+    {
+        _cityService = cityService;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> All()
+    {
+        var cities = await _cityService.GetAllCitiesAsync();
+        return Json(cities);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CityCreateModel model)
+    {
+        var cityId= await _cityService.CreateCityAsync(model);
+        return Json(cityId);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var result = await _cityService.DeleteCityAsync(id);
+        if (result == false)
+            return RedirectToAction("Index", "Error", new ErrorModel
+            {
+                ErrorMessage = "Удалить невозможно, возможно город используется в рейсе",
+                ActionName = "Index",
+                ControllerName = "City"
+            });
+        return RedirectToAction("Index");
+    }
+}
