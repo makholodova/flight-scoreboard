0647f628bd63f004bc86bb7e2b9051470a8b329c
Marina Kholodova <marinaholodova@gmail.com>
2024-01-14 16:45:32 +0300

    city for vue

---
+++ src/FlightScoreboardApi/Controllers/CityController.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using FlightScoreboardData.Services;
+using FlightScoreboardData.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightScoreboardApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CityController : ControllerBase
+{
+	private readonly ICityService _cityService;
+
+	public CityController(ICityService cityService)
+	{
+		_cityService = cityService;
+	}
+
+	[HttpGet(Name = "All")]
+	public async Task<IActionResult> All()
+	{
+		var cities = await _cityService.GetAllCitiesAsync();
+		return Ok(cities);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Create([FromBody] CityCreateModel model)
+	{
+		var cityId = await _cityService.CreateCityAsync(model);
+		return Ok(cityId);
+	}
+
+	[HttpDelete]
+	[ProducesResponseType(200)]
+	[ProducesResponseType(400)]
+	public async Task<IActionResult> Delete(int id)
+	{
+		var result = await _cityService.DeleteCityAsync(id);
+		if (result == false)
+			return BadRequest("City is not found");
+		return Ok();
+	}
+}
